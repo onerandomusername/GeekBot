e2bba7093e030bbec38ff6713d138aedc6a77fc7
aru <genericusername414+git@gmail.com>
2021-03-30 23:57:25 -0400

    add fancy embed support

--- bot/cogs/meta.py
+++ bot/cogs/meta.py
@@ -5,6 +5,8 @@ import typing
 from pathlib import Path
 
 import discord
+from discord.errors import DiscordException
+#from discord.utils import DISCORD_EPOCH
 import pygit2
 import verboselogs
 from bot import Bot
@@ -33,12 +35,13 @@ class SomeCommands(commands.Cog):
     async def _invite(self, ctx: commands.Context):
         """Return the bot invite, and a notice if it is not public."""
         app_info: discord.AppInfo = await self.bot.application_info()
-        ctx.reply(f'Invite me here!\n<{self.bot.invite_link.format(self.bot.user.id)}> \n'
-                  '**Warning!** I am currently not a public bot and may never be!' if not app_info.bot_public else ''
-                  )
+        await ctx.reply(f'Invite me here!\n<{self.bot.invite_link.format(self.bot.user.id)}> \n'
+                        '**Warning!** I am currently not a public bot and may never be!' if not app_info.bot_public else ''
+                        )
 
     @commands.command(name='source', aliases=['src', 'code'])
     async def source(self, ctx: commands.Context, source_item: str = None):
+        '''Get a github link to the source code of a command.'''
         if source_item is None:
             await ctx.send(github_link)
             return
@@ -46,11 +49,12 @@ class SomeCommands(commands.Cog):
 
         if cmd is None:
             raise commands.CommandError('Couldn\'t find command.')
-        source_lines = inspect.getsourcelines(cmd.callback)
-        source_file = inspect.getsourcefile(cmd.callback)
+        callback = cmd.callback
+        source_lines = inspect.getsourcelines(callback)
+        source_file = inspect.getsourcefile(callback)
 
         length, start_line = len(source_lines[0]), source_lines[1]
-        end_line = start_line + length
+        end_line = start_line + length - 1
         repo = pygit2.Repository('.git')
         tree = None
         for branch in list(repo.branches.local):
@@ -58,10 +62,20 @@ class SomeCommands(commands.Cog):
             if b.is_head():
                 tree = branch
                 break
-
-        link = f'{github_link}/tree/{tree}/{Path(source_file).relative_to(str(Path.cwd()))}'
+        source_file = str(Path(source_file).relative_to(str(Path.cwd())))
+        link = f'{github_link}/tree/{tree}/{source_file}'
         link += f'#L{start_line}-L{end_line}'
-        await ctx.send(link)
+        if not ctx.channel.permissions_for(ctx.me) >= discord.Permissions(embed_links=True, attach_files=True):
+            await ctx.send(link)
+            return
+        
+        #make a fancy embed!
+        e = discord.Embed()
+        e.description=f'{cmd.short_doc}\n\n' \
+                    f'[View on github.]({link})'
+        e.title = source_item
+        e.set_footer(text=f'/{source_file}')
+        await ctx.send(embed=e)
 
 
 def setup(bot: Bot):
