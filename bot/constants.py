a4acfd45c579854b7e0d9e42e905337ca83757b0
aru <genericusername414+git@gmail.com>
2021-04-04 13:25:13 -0400

    add github class to constants

---
+++ bot/constants.py
@@ -0,0 +1,28 @@
+MESSAGE_LIMIT = 2000
+
+
+class Github:
+    api_link = "https://api.github.com"
+
+    class Me:
+        org = "onerandomusername"
+        repo = "geekbot"
+        html_link = f"https://github.com/{org}/{repo}"
+
+    # Me = Me()
+
+
+class Colors:
+    blue = 0x0279FD
+    bright_green = 0x01D277
+    dark_green = 0x1F8B4C
+    orange = 0xE67E22
+    pink = 0xCF84E0
+    purple = 0xB734EB
+    soft_green = 0x68C290
+    soft_orange = 0xF9CB54
+    soft_red = 0xCD6D6D
+    yellow = 0xF9F586
+    python_blue = 0x4B8BBE
+    python_yellow = 0xFFD43B
+    grass_green = 0x66FF00
