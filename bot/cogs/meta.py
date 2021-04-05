68024553d23e6e9abf1233e69c73989314fcf729
aru <genericusername414+git@gmail.com>
2021-04-04 21:59:35 -0400

    use ast instead of self parsing

--- bot/cogs/meta.py
+++ bot/cogs/meta.py
@@ -1,3 +1,4 @@
+import ast
 import base64
 import inspect
 import logging
@@ -14,7 +15,6 @@ from constants import Github
 from discord.errors import DiscordException
 from discord.ext import commands
 from utils.file import create_file_obj
-import pprint
 
 github_link = Github.Me.html_link
 log: verboselogs.VerboseLogger = logging.getLogger(__name__)
@@ -59,48 +59,28 @@ class SomeCommands(commands.Cog):
                 raise commands.CommandError("Command Not [on github].")
             json = await resp.json()
             source_code = base64.b64decode(json["content"]).decode("utf-8")
+            line_num = None
+            the_ast = ast.parse(source_code)
+            for node in ast.walk(the_ast):
+                if isinstance(node, ast.AsyncFunctionDef) or isinstance(
+                    node, ast.FunctionDef
+                ):
+                    if getattr(node, "name", "") == command:
+                        line_num = node.lineno - len(node.decorator_list)
 
-            # this line needs to use a regex at some point, to find either where it defines the name, or where it defines the method
-            cmd_line_POS = source_code.find(f"def {command}")
+                        end_line_num = node.end_lineno
+                        break
 
-            if not cmd_line_POS >= 0:
-                raise commands.CommandError("Command is not on github.")
+            if line_num is None:
+                raise commands.CommandError("Command Not [on github].")
 
-            # find the beginning of this command's source.
-            start_of_cmd = cmd_line_POS - source_code[cmd_line_POS::-1].find("\n\n")
-            start_of_cmd += 1  # we checked for \n\n, this makes it the second `\n`
-            line_num = source_code[:start_of_cmd].count("\n")
-            split_source = source_code[start_of_cmd:].split("\n")
-            # await ctx.send(
-            #     "split_source", file=create_file_obj("\n".join(split_source), ext="py")
-            # )
-            original_indentation = " " * (
-                len(split_source[0]) - len(split_source[0].lstrip())
-            )
-            end = line_num
-            func_indent = original_indentation + " " * 4
-            skip_lines = 2
-            for i, line in enumerate(split_source[skip_lines:]):
-                if bool(line.lstrip()) and not line.startswith(func_indent):
-                    end += i + skip_lines
-                    break
-
-            # await ctx.send(f"line_num : {line_num}\nend: {end}")
-            # await ctx.send(
-            #     file=create_file_obj(
-            #         textwrap.dedent(
-            #             "\n".join(source_code.splitlines()[line_num : end + 1])
-            #         ),
-            #         ext="py",
-            #     )
-            # )
             return "{0}/{1}/{2}/tree/main/{3}#L{4}-L{5}".format(
                 Github.base_link,
                 Github.Me.org,
                 Github.Me.repo,
                 file.lstrip("/"),
-                line_num + 1,
-                end - 1,
+                line_num,
+                end_line_num,
             )
 
     @commands.command(name="source", aliases=["src", "code"])
@@ -130,7 +110,7 @@ class SomeCommands(commands.Cog):
         link = f"{github_link}/tree/{tree}/{source_file}"
         link += f"#L{start_line}-L{end_line}"
         link = await self._get_github_source(
-            ctx, cmd.qualified_name, source_file, length
+            ctx, callback.__name__, source_file, length
         )
         if not ctx.channel.permissions_for(ctx.me) >= discord.Permissions(
             embed_links=True, attach_files=True
